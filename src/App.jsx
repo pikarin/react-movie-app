e963f5624f5f7d9410683574101a63458d09dd39
M. Aditia Firmansyah <aditia.pikarin@gmail.com>
2025-05-07 09:21:06 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <main>
+      <div className="pattern"></div>
+
+      <div className="wrapper">
+        <header>
+          <h1>Find Movies You'll Enjoy Without the Hassle</h1>
+        </header>
+      </div>
+    </main>
+  )
+}
+
+export default App
